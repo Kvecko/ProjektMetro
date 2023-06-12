b4b6618ecdd3792693881ecf74f292b914bb1003
tomas.pabian <tomas.pabian@VT1-05>
2023-06-12 10:39:40 +0200

    Přidejte soubory projektu.

---
+++ ProjektMetro/LinkaMetra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektMetro
+{
+    internal class LinkaMetra
+    {
+        public string nazevLinky { get; set; }
+        public string[] poleStanice { get; set; }
+        public bool aktivni { get; set; }
+    }
+}
