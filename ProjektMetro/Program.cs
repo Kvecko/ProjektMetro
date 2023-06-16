d01e460f519f1bbbca61b15eedc399e4d1e282db
Tomáš Pabián <tomas.pabian@educhem.cz>
2023-06-16 12:54:35 +0200

    Zkurveno a dojebano

--- ProjektMetro/Program.cs
+++ ProjektMetro/Program.cs
@@ -1,12 +1,19 @@
 ﻿using ProjektMetro;
+using Spectre.Console;
 
 string[] poleLinkaA = { "Nemocnine Motol", "Petříny", "Nádraží Veleslavín", "Bořislavka", "Dejvcická", "Hradčanská", "Malostranská", "Staroměstská", "Můstek", "Muzeum", "Náměstí Míru", "Jířího z Poděbrad", "Flora", "Želivského", "Strašnická", "Skalka", "Depo Hostivař" };
 string[] poleLinkaB = { "Zličín", "Stodůlky", "Luka", "Lužiny", "Hůrka", "Nové Butovice", "Jinonice", "Radlická", "Smíchovské nádraží", "Anděl", "Karlovo náměstí", "Národní Třída", "Můstek", "Náměstí Republiky", "Florenc", "Křižíkova", "Invalidovna", "Palmovka", "Českomoravská", "Vysočanská", "Kolbenova", "Hloubětín", "Rajská zahrada", "Černý most"};
 string[] poleLinkaC = { "Letňany", "Prosek", "Střižíkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní nádraží", "Muzeum", "I. P. Pavlova", "Vyšehrad", "Pražského Povstání", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "Chodov", "Opatov", "Háje" };
 string[] poleLinkaD = { "Náměstí Míru", "Náměstí Bří synků", "Pankrác", "Olbrachtova", "Nádraží Krč", "Nemocnice Krč", "Nové Dvory", "Libuš", "Písnice", "Depo Písnice"};
 
-Console.WriteLine("Zadej název linky (A-D)");
-string volbaLinky = Console.ReadLine().ToUpper();
+var volbaLinky = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Vyber si linku")
+        .PageSize(10)
+        .AddChoices(new[] {
+            "A", "B", "C", "D"
+        }));
+
 if (volbaLinky == "A")
 {
     LinkaMetra linkaA = new LinkaMetra()
@@ -18,8 +25,14 @@ if (volbaLinky == "A")
     };
     linkaA.NastavLinku();
 
-    Console.WriteLine("1 - Vypsat všechny stanice \n2 - Vypsat jenom první a poslední \n3 - Zjistit čas mezi stanicemi");
-    string volbaVypsani = Console.ReadLine();
+    var volbaVypsani = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .PageSize(10)
+        .HighlightStyle(Color.Green3)
+        .AddChoices(new[] {
+            "Vypsat všechny stanice", "Vypsat jenom první a poslední", "Zjistit čas mezi stanicemi"
+        }));
+
     if (volbaVypsani == "1")
     {
         linkaA.VypisStanice();
@@ -44,8 +57,14 @@ else if (volbaLinky == "B")
     };
     linkaB.NastavLinku();
 
-    Console.WriteLine("1 - Vypsat všechny stanice \n2 - Vypsat jenom první a poslední \n3 - Zjistit čas mezi stanicemi");
-    string volbaVypsani = Console.ReadLine();
+    var volbaVypsani = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .PageSize(10)
+        .HighlightStyle(Color.Yellow1)
+        .AddChoices(new[] {
+            "Vypsat všechny stanice", "Vypsat jenom první a poslední", "Zjistit čas mezi stanicemi"
+        }));
+
     if (volbaVypsani == "1")
     {
         linkaB.VypisStanice();
@@ -70,8 +89,14 @@ else if (volbaLinky == "C")
     };
     linkaC.NastavLinku();
 
-    Console.WriteLine("1 - Vypsat všechny stanice \n2 - Vypsat jenom první a poslední \n3 - Zjistit čas mezi stanicemi");
-    string volbaVypsani = Console.ReadLine();
+    var volbaVypsani = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .PageSize(10)
+        .HighlightStyle(Color.Red1)
+        .AddChoices(new[] {
+            "Vypsat všechny stanice", "Vypsat jenom první a poslední", "Zjistit čas mezi stanicemi"
+        }));
+
     if (volbaVypsani == "1")
     {
         linkaC.VypisStanice();
@@ -96,8 +121,14 @@ else if (volbaLinky == "D")
     };
     linkaD.NastavLinku();
 
-    Console.WriteLine("1 - Vypsat všechny stanice \n2 - Vypsat jenom první a poslední \n3 - Zjistit čas mezi stanicemi");
-    string volbaVypsani = Console.ReadLine();
+    var volbaVypsani = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .PageSize(10)
+        .HighlightStyle(Color.Blue1)
+        .AddChoices(new[] {
+            "Vypsat všechny stanice", "Vypsat jenom první a poslední", "Zjistit čas mezi stanicemi"
+        }));
+
     if (volbaVypsani == "1")
     {
         linkaD.VypisStanice();
